82008fbe9fc3588af0b7ac81fd0afdb128d7fc7e
Gaurav-Wagh1 <gauravwagh0506@gmail.com>
2024-01-25 13:17:35 +0530

    Added error files

---
+++ src/utils/errors/app-error.js
@@ -0,0 +1,16 @@
+class AppError extends Error {
+    constructor(
+        name,
+        message,
+        explanation,
+        statusCode
+    ) {
+        super();
+        this.name = name;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+module.exports = AppError;
